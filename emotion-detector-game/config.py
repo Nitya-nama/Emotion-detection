a728b7a8a1e3646d317282156b8bee760da69026
Nitya Phaneesh Chandra Nama <nityanama101@gmail.com>
2025-11-01 21:41:56 +0530

    Modify MongoDB connection and add collection
    Updated MongoDB connection URI and added collection reference.

--- emotion-detector-game/config.py
+++ emotion-detector-game/config.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://flaskuser:flask123@cluster0.xmpclqw.mongodb.net/?appName=Cluster0")
+client = MongoClient("your-mongodb-atlas-uri ")
 db = client["emotion_game"]
+
 collection = db["emotions"]
